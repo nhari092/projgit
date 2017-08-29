787780828aeaa0fe8953c4f4d1cc262b4cec5dcf
hari <hari@hari-PC>
2017-08-29 11:48:55 +0530

    //this is first push

---
+++ src/com/nt/TestGit.java
@@ -0,0 +1,28 @@
+package com.nt;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class TestGit {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner s=null;
+		String date=null;
+		java.util.Date ud1=null;
+		SimpleDateFormat sdf1=null;
+		long ms=0;
+		java.sql.Date sqd1=null;
+		s=new Scanner(System.in);
+		System.out.println("ENTER DATE VALUE");
+		date=s.next();//DD-MM-YYYY
+		 sdf1=new SimpleDateFormat("dd-MM-yyyy");
+		 ud1=sdf1.parse(date);
+		System.out.println(" DATE VALUE="+date);
+		ms=ud1.getTime();
+		sqd1=new java.sql.Date(ms);
+		System.out.println(" SQL DATE VALUE="+sqd1);
+		s.close();
+			}
+
+		}
